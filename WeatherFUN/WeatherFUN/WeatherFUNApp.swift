4a53a6e4ed9e3dade28997467b774d9e6c4061e5
jjwon2149 <jjwon2149@gmail.com>
2024-04-30 15:26:56 +0900

    chore: initial commit

---
+++ WeatherFUN/WeatherFUN/WeatherFUNApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFUNApp.swift
+//  WeatherFUN
+//
+//  Created by 정종원 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFUNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
