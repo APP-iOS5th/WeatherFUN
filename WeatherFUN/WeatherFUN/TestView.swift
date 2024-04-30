b2af1e00a18bb1d980b8519c53aa7ab2fab8471b
sixteenis <psm0103907@naver.com>
2024-04-30 16:26:13 +0900

    메인 가로 뷰

---
+++ WeatherFUN/WeatherFUN/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  WeatherFUN
+//
+//  Created by 박성민 on 4/30/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        MainHorizontalScrollView()
+            .frame(width: 360)
+    }
+}
+
+#Preview {
+    TestView()
+}
