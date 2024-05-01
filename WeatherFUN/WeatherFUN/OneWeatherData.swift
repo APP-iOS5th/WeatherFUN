a47c1fd992ab4ada2903fcc943a7a1f356ed67b2
jjwon2149 <jjwon2149@gmail.com>
2024-05-01 18:44:49 +0900

    refactor: struct rename

--- WeatherFUN/WeatherFUN/OneWeatherData.swift
+++ WeatherFUN/WeatherFUN/OneWeatherData.swift
@@ -6,24 +6,43 @@
 //
 
 import Foundation
-import CoreLocation
 
-struct OneWeatherData {
-    var locationName: String
-    var temperature: Double
-    var condition: String
-}
+//struct OneWeatherData {
+//    var locationName: String
+//    var temperature: Double
+//    var temp_min: Double
+//    var temp_max: Double
+//    var condition: String
+//}
+//
+//struct OneWeatherResponse: Codable {
+//    let name: String
+//    let main: OneMainWeather
+//    let weather: [OneWeather]
+//}
+//
+//struct OneMainWeather: Codable {
+//    let temp: Double
+//}
+//
+//struct OneWeather: Codable {
+//    let description: String
+//}
 
-struct OneWeatherResponse: Codable {
+// MARK: - Welcome
+struct OneWeatherData: Codable {
+    let weather: [OneDayWeather]
+    let main: OneDayMain
     let name: String
-    let main: OneMainWeather
-    let weather: [OneWeather]
 }
 
-struct OneMainWeather: Codable {
-    let temp: Double
+// MARK: - Main
+struct OneDayMain: Codable {
+    let temp, temp_min, temp_max: Double?
 }
 
-struct OneWeather: Codable {
-    let description: String
+// MARK: - Weather
+struct OneDayWeather: Codable {
+    let id: Int
+    let main, description: String
 }
