1f738d8c96b3e0808e1c23f52f2f2487d9de41fb
sixteenis <psm0103907@naver.com>
2024-05-02 13:26:29 +0900

    날씨 이미지 설정

--- WeatherFUN/WeatherFUN/ViewModel/FiveWeatherViewModel.swift
+++ WeatherFUN/WeatherFUN/ViewModel/FiveWeatherViewModel.swift
@@ -11,9 +11,9 @@ import SwiftUI
 class FiveWeatherViewModel: ObservableObject {
     
     @Published var fiveDayWeatherNetwork = FiveDayWeatherNetwork()
-    @Published var locationManager: LocationManager = LocationManager()
+    //@Published var locationManager: LocationManager = LocationManager()
     @Published var fiveDayWeatherDates: [FiveDayWeatherModel] = []
-    
+    @Published var today: [FiveDayWeatherModel] = []
     init() {
         LocationManager.shared.fiveDayWeatherViewModel = self
         LocationManager.shared.requestLocation()
@@ -23,6 +23,16 @@ class FiveWeatherViewModel: ObservableObject {
         for i in fiveDayWeatherNetwork.weatherDatas[0].list {
             guard let time = i.dt_txt else {return}
             fiveDayWeatherDates.append(FiveDayWeatherModel(time: time, weather: i.weather[0].main, temp: i.main.temp))
+        }
+        let todayTime = Array(fiveDayWeatherDates[0].time) //8 ,9
+        print(todayTime)
+        for d in fiveDayWeatherDates {
+            let t = Array(d.time)
+            if todayTime[9] == t[9] {
+                today.append(FiveDayWeatherModel(time: (String(t[11]) + String(t[12])), weather: d.weather, temp: d.temp))
+            }else{
+                break
+            }
         }
        
     }
