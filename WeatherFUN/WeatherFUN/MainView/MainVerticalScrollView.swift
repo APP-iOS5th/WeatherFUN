dcfdbaaa82f9ce191b0eeb102253b34844882a97
uunwon <chea2089@naver.com>
2024-05-03 10:59:48 +0900

    요일 이름 변경

--- WeatherFUN/WeatherFUN/MainView/MainVerticalScrollView.swift
+++ WeatherFUN/WeatherFUN/MainView/MainVerticalScrollView.swift
@@ -28,8 +28,9 @@ struct MainVerticalScrollView: View {
                     .padding(.horizontal, 10)
                 
                 HStack {
-                    Text("\(day.time)일")
+                    Text("\(day.time)")
                         .bold()
+                        .padding(.leading, 3.0)
                     Spacer()
                     
                     Image(systemName: day.weatherIcon)
